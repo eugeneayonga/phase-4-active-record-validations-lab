06be2cd5cd5583302ed2c8d91b59a6c1ba1de669
Eugene <eugeneayonga@gmail.com>
2022-10-02 19:21:05 +0300

    commit tree 9a098df16865ce816e8dfd317c7daafa3cb74bf8

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ApplicationRecord
+    
+    # All authors have a name
+    # All authors have a unique name
+    validates :name, presence: true, uniqueness: true
+
+    # All authors have a phone_number of exactly 10 digits
+    validates :phone_number, length: { is: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    
+    # All posts have a title
+    validates :title, presence: true
+
+    # Post content is at least 250 characters long
+    validates :content, length: { minimum: 250 }
+
+    # Post summary is a maximum of 250 characters
+    validates :summary, length: { maximum: 250 }
+
+    # Post category is either Fiction or Non-Fiction
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    # Custom validator that ensures the title is sufficiently clickbait-y
+    # The validator should add a validation error if the title does not contain: "Won't Believe", "Secret", "Top [number]", "Guess"
+    validates :title, exclusion: { in: ["True Facts"] }
 end
